9df638bbce6538bfbaeea18dfc7b784dcb08fc6e
DroopyHyperion5 <64291634+DroopyHyperion5@users.noreply.github.com>
2021-09-20 18:54:55 -0500

    Gato
    se modifico el .js 0.0.2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require("http");//variable
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.write(req.url);
+    res.end();
+
   })
   .listen(PORT);
