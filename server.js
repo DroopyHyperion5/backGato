6f0ed48f6990e18588563eafcc2864ec0cbc4731
DroopyHyperion5 <64291634+DroopyHyperion5@users.noreply.github.com>
2021-09-20 19:14:48 -0500

    Gato en linea 0.0.3
    se agrego mensaje de Horror y bueno.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const PORT = process.env.PORT || 3000;
-var http = require("http");//variable
+var http = require("http"); //variable
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write(req.url);
-    res.end();
 
+    if (req.url.lastIndexOf("juega")) {
+      res.write("Let´s Go");
+    }
+    else{
+        res.write("Game Over");
+    }
+    res.end();
   })
   .listen(PORT);
