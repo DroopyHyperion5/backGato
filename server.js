05f28c9f469cd3dd5f19fb31b8f606ff8b40b758
DroopyHyperion5 <64291634+DroopyHyperion5@users.noreply.github.com>
2021-09-20 20:02:11 -0500

    Gato 0.0.7

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const PORT = process.env.PORT || 3000;
 var http = require("http"); //variable
+var tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
 
     if (req.url.lastIndexOf("juega") !== -1) {
-      //res.write("Juega");
-      var inicio = req.url.lastIndexOf("juega")+5;
-      var fin = inicio + 8 + 9;//
+      var inicio = req.url.lastIndexOf("juega") + 5;
+      var fin = inicio + 8 + 9; //
       var text = req.url;
       text = text.substring(inicio, fin);
-      res.write(text);
+      var valores = text.split(",");
+      for (var i = 0; i < 9; i++) {
+        if (valores[i] >= 0 && valores[i] <= 2) {
+          valores[i] = parseInt(valores[i]);
+        } else {
+          valores[i] = 0;
+        }
+      }
+      tablero = valores;
+      res.write(JSON.stringify(tablero));
     } else {
-      res.write("Esto No Es Una Jugada");
+      res.write(JSON.stringify(tablero));
     }
+
     res.end();
   })
   .listen(PORT);
